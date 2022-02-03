87a4c7eab1ba3d3ad8e5484d3afefb679fa99c65
Rodrigo Nicola <rodrigo.nicola0@gmail.com>
2022-02-03 14:38:53 -0300

    Add easy declaration of multi-check problems.
    * Refactor runner function so that it is easier to run several
    checks per problem.
    
    * Change the expected yaml config file.

--- tests/check404.py
+++ tests/check404.py
@@ -6,37 +6,48 @@ import verboselogs
 
 logger = verboselogs.VerboseLogger(__name__)
 log_fmt = "%(levelname)-10s %(message)s"
-coloredlogs.install(level='DEBUG', logger=logger, fmt=log_fmt)
+coloredlogs.install(level='INFO', logger=logger, fmt=log_fmt)
 
 
-def runner(checks):
-    for name, check in checks.items():
+def run_check(command, stdin, prompts):
+    logger.info(f">>>> Starting check with input: {stdin}.")
+    logger.debug(f"Spawning process {command}")
+    child = pexpect.spawn(command)
+    logger.debug(f"Prompts detected: {prompts}")
+    if prompts:
+        for prompt, response in zip(prompts, stdin):
+            logger.verbose(f"Waiting for prompt '{prompt}'")
+            index = child.expect([prompt, pexpect.TIMEOUT], timeout=1)
+            logger.verbose("Prompt received!")
+            if index:
+                logger.critical("Timed out waiting for prompt.")
+                logger.warning("Expected pattern: {prompt}")
+                exit(2)
+            logger.verbose(f"Sending '{response}'")
+            child.sendline(response)
+            logger.verbose("Response sent. Flushing stdout.")
+            child.readline()
+        logger.verbose("Waiting for stdout.")
+    stdout = child.readline().decode('utf-8').replace("\r\n", "")
+    return stdout
+
+
+def runner(problem_set):
+    for name, problem in problem_set.items():
         logger.info(f">> Starting check on {name}")
-        child = pexpect.spawn(check['command'])
-        logger.debug(f"Spawning process {check['command']}")
-        prompts = check['stdin']['prompts']
-        logger.debug(f"Prompts detected: {prompts}")
-        responses = check['stdin']['responses']
-        logger.debug(f"Responses detected: {responses}")
-        if prompts:
-            for prompt, response in zip(prompts, responses):
-                logger.verbose(f"Waiting for prompt '{prompt}'")
-                index = child.expect([prompt, pexpect.TIMEOUT], timeout=1)
-                logger.verbose("Prompt received!")
-                if index:
-                    logger.critical("Timed out waiting for prompt.")
-                    logger.warning("Expected pattern: {prompt}")
-                    exit()
-                logger.verbose(f"Sending '{response}'")
-                child.sendline(response)
-                logger.verbose("Response sent.")
-                child.readline()
-        stdout = child.readline().decode('utf-8').replace("\r\n", "")
-        if stdout == check['stdout']:
-            logger.success("Test passed")
-        else:
-            logger.error("Test failed.")
-            logger.warning(f"Expected '{stdout}'. Got '{check['stdout']}'")
+        prompts = problem['prompts']
+        command = problem['command']
+        for i, stdout in enumerate(problem['stdout']):
+            stdin = problem['stdin'][i]
+            hint = problem['hints'][i]
+            out = run_check(command, stdin, prompts)
+            if stdout == out:
+                logger.success("Test passed")
+            else:
+                logger.error("Test failed.")
+                logger.warning(f"Expected '{stdout}'. Got '{out}'")
+                if hint != "":
+                    logger.notice(f"Hint: {hint}")
 
 
 def main():
