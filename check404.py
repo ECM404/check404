3484938834206230a804d2050aed4524e702fe1d
Rodrigo Nicola <rodrigo.nicola0@gmail.com>
2022-02-08 11:34:11 -0300

    Fix crash when there is no .out file
    * Simply use os module to check for existing file. If it does not
    exist, send a critical log message and skip that test altogether.

--- check404.py
+++ check404.py
@@ -16,6 +16,11 @@ coloredlogs.install(level='INFO', logger=logger, fmt=log_fmt)
 
 
 def run_check(command, stdin, prompts):
+    file = command.replace("./", "")
+    if not os.path.isfile(file):
+        logger.critical(f"O arquivo {file} não existe.")
+        logger.warning("Verifique se não houve erro de compilação.")
+        return "nofile_err"
     logger.info(f"Verificando com input: {stdin}.")
     logger.debug(f"Iniciando processo com comando: {command}.")
     child = pexpect.spawn(command)
@@ -53,6 +58,8 @@ def runner(problem_set):
             stdin = problem['stdin'][i]
             hint = problem['hints'][i]
             out = run_check(command, stdin, prompts)
+            if out == "nofile_err":
+                break
             if stdout == out:
                 logger.success("Teste concluído com sucesso =)")
             else:
