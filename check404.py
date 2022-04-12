a1462889109946327bb5f3b22682a0da50a6309d
Rodrigo Nicola <rnicola@maua.br>
2022-04-12 17:57:12 -0300

    Add simple IO Stream check

--- check404.py
+++ check404.py
@@ -1,7 +1,58 @@
 import subprocess
 from subprocess import PIPE
+from dataclasses import dataclass
+from collections import namedtuple
+from enum import Enum
 
-process = subprocess.Popen("./a.out", stdin=PIPE, stdout=PIPE, encoding='utf-8')
-process.stdin.write("5\n")
-stdout, stderr = process.communicate()
-print(stdout)
+GREEN = "\033[1;32;49m"
+RED = "\033[1;31;49m"
+WHITE = "\033[1;37;49m"
+TIMEOUT = 2
+
+CheckResult = namedtuple("CheckResult", ['state', 'msg'])
+
+
+class CheckState(Enum):
+    ERROR = 1
+    PASSED = 2
+    FAILED = 3
+
+
+@dataclass
+class Check():
+    file: str
+    input: str
+    expect: str
+
+    def run(self) -> CheckResult:
+        try:
+            process = subprocess.Popen([f"./{self.file}"],
+                                       stdin=PIPE,
+                                       stdout=PIPE,
+                                       encoding='utf-8')
+        except OSError as e:
+            msg = f"O arquivo {e.filename[2:]} não foi encontrado."
+            return CheckState.ERROR, msg
+        try:
+            output, stderr = process.communicate(input=self.input,
+                                                 timeout=TIMEOUT)
+        except subprocess.TimeoutExpired as e:
+            msg = f"Programa não respondeu após {e.timeout} segundos."
+            return CheckState.ERROR, msg
+
+        if self.expect in output:
+            msg = "Teste concluído com sucesso!"
+            return CheckState.PASSED, msg
+        else:
+            msg = f"Não passou. Esperava encontrar '{self.expect}' na saída."
+            return CheckState.FAILED, msg
+
+
+def main():
+    check = Check(file='b.out', input='5', expect='5')
+    result = check.run()
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
--- tests/teste.c
+++ tests/teste.c
@@ -2,9 +2,6 @@
 
 int main (int argc, char *argv[])
 {
-  int a;
-  printf("Digite o valor de a: ");
-  scanf("%i", &a);
-  printf("Valor digitado: %i", a);
+  printf("600");
   return 0;
 }
